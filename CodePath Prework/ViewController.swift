dd75aea1338f60318ac4f7f10e828dafc6771ff7
yulanwang <yulanw16@gmail.com>
2023-12-28 15:20:12 -0500

    pushed
    push

---
+++ CodePath Prework/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CodePath Prework
+//
+//  Created by Yulan Wang on 12/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+        
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+              
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+}
